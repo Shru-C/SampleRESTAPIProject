42a988f88df6601cd8c2860e6bea71673344f8ab
Shruthi Chikkadoddaiah <Shruthi.Chikkadoddaiah@starcompliance.com>
2021-09-28 12:41:46 +0530

    RestAPITest

---
+++ SampleRESTAPIProject/UserCreation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleRESTAPIProject
+{
+    public class UserCreation
+    {
+        public string name { get; set; }
+        public string job { get; set; }
+    }
+
+}
